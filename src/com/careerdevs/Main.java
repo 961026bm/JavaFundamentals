5c203040f675b8bba26f5367104742ffcd4e99d8
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-06 20:49:26 -0500

    mosh A-01-03 done

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /* Function is a block of code that performs a task
+         ReturnType Name() {
+         ...
+         }
+         void is a returnType in java
+         parenthesis  is used to pass the parameters
+         before the code block
+         every java must have an entry point and it is called main()
+         when java is function is called the code in the main is being executed
+         this main() does not exist on its own it belongs to a class
+          defining a Class ==> a container for related functions used to organize codes.
+          an access modifier is a special keyword that determines if other methods
+          and functions can access this classes and methods. i.e public or private
+
+          public class Main {
+            public void main () {
+            ...
+            }
+          }
+
+    In java there different naming conventions that are used i.e
+    PascalNamingConvention for classes
+    camelNamingConvention for methods
+
+         * */
     }
 }
