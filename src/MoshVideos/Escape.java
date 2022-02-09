ea40d5a925a118cba01d3731b84404cc868b2433
961026bm <olubunmiosunde.student@careerdevs.com>
2022-02-08 19:03:31 -0500

    mosh A-02-07 done

---
+++ src/MoshVideos/Escape.java
@@ -0,0 +1,12 @@
+package MoshVideos;
+
+public class Escape {
+
+    public static void main(String[] args) {
+        String message = "Joe Biden";
+        message = "Joe \"Biden\"";
+        message = "Joe \n Biden";
+        message = "Joe: \t\nBiden";
+        System.out.println(message);
+    }
+}
