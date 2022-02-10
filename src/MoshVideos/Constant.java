2be6dd5d752c1daf457f4960ededccb50ef694d4
961026bm <olubunmiosunde.student@careerdevs.com>
2022-02-10 18:02:48 -0500

    mosh A-02-10 done

---
+++ src/MoshVideos/Constant.java
@@ -0,0 +1,10 @@
+package MoshVideos;
+
+public class Constant {
+
+    public static void main(String[] args) {
+        final float pi = 3.14F; // F is needed in front of the number to represent float
+        //Final in java  does the same thing as Const in javascript
+
+    }
+}
